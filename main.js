0ee61984a789c5113ff4e813e0e903e0ec7f159f
Lubna <muspaksamsu@gmail.com>
2024-07-01 22:39:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Greetings: Start with the array you used in Exercise 11, 
+//but instead of just printing each person’s name, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+var names = ["Lubna", "Kanwal", "Aqsa", "Huma"];
+console.log("Birthday Invetation,".concat(names[0]));
+console.log("Birthday Invetation,".concat(names[1]));
+console.log("Birthday Invetation,".concat(names[2]));
+console.log("Birthday Invetation,".concat(names[3]));
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Greetings: Start with the array you used in Exercise 11, 
+//but instead of just printing each person’s name, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+
+let names: string[] = ["Lubna","Kanwal","Aqsa","Huma"];
+
+console.log(`Birthday Invetation,${names[0]}`);
+console.log(`Birthday Invetation,${names[1]}`);
+console.log(`Birthday Invetation,${names[2]}`);
+console.log(`Birthday Invetation,${names[3]}`);
